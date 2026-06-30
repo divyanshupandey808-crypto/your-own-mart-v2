960b9c11c2493a850302cb4b6964436ccc587a87
divyanshupandey808-crypto <divyanshupandey808@gmail.com>
2026-06-29 19:17:50 -0700

    Add product listing and functionality in script.js

---
+++ script.js
@@ -0,0 +1,358 @@
+const products = [
+{ name: "YOM001", category: "women dress", image: "yom001.jpg" },
+{ name: "YOM002", category: "women dress", image: "yom002.jpg" },
+{ name: "YOM003", category: "women dress", image: "yom003.jpg" },
+{ name: "YOM004", category: "women dress", image: "yom004.jpg" },
+{ name: "YOM005", category: "women dress", image: "yom005.jpg" },
+{ name: "YOM006", category: "women dress", image: "yom006.jpg" },
+{ name: "YOM007", category: "women dress", image: "yom007.jpg" },
+{ name: "YOM008", category: "women dress", image: "yom008.jpg" },
+{ name: "YOM009", category: "women dress", image: "yom009.jpg" },
+{ name: "YOM010", category: "women dress", image: "yom010.jpg" },
+{ name: "YOM011", category: "women dress", image: "yom011.jpg" },
+{ name: "YOM012", category: "women dress", image: "yom012.jpg" },
+{ name: "YOM013", category: "women dress", image: "yom013.jpg" },
+{ name: "YOM014", category: "women dress", image: "yom014.jpg" },
+{ name: "YOM015", category: "women dress", image: "yom015.jpg" },
+{ name: "YOM016", category: "women dress", image: "yom016.jpg" },
+{ name: "YOM017", category: "women dress", image: "yom017.jpg" },
+{ name: "YOM018", category: "women dress", image: "yom018.jpg" },
+{ name: "YOM019", category: "women dress", image: "yom019.jpg" },
+{ name: "YOM020", category: "women dress", image: "yom020.jpg" },
+{ name: "YOM021", category: "women dress", image: "yom021.jpg" },
+{ name: "YOM022", category: "women dress", image: "yom022.jpg" },
+{ name: "YOM023", category: "women dress", image: "yom023.jpg" },
+{ name: "YOM024", category: "women dress", image: "yom024.jpg" },
+{ name: "YOM025", category: "women dress", image: "yom025.jpg" },
+{ name: "YOM026", category: "women dress", image: "yom026.jpg" },
+{ name: "YOM027", category: "women dress", image: "yom027.jpg" },
+{ name: "YOM028", category: "women dress", image: "yom028.jpg" },
+{ name: "YOM029", category: "women dress", image: "yom029.jpg" },
+{ name: "YOM030", category: "women dress", image: "yom030.jpg" },
+{ name: "YOM031", category: "women dress", image: "yom031.jpg" },
+{ name: "YOM032", category: "women dress", image: "yom032.jpg" },
+{ name: "YOM033", category: "women dress", image: "yom033.jpg" },
+{ name: "YOM034", category: "women dress", image: "yom034.jpg" },
+{ name: "YOM035", category: "women dress", image: "yom035.jpg" },
+{ name: "YOM036", category: "women dress", image: "yom036.jpg" },
+{ name: "YOM037", category: "women dress", image: "yom037.jpg" },
+{ name: "YOM038", category: "women dress", image: "yom038.jpg" },
+{ name: "YOM039", category: "women dress", image: "yom039.jpg" },
+{ name: "YOM040", category: "women dress", image: "yom040.jpg" },
+{ name: "YOM041", category: "women dress", image: "yom041.jpg" },
+{ name: "YOM042", category: "women dress", image: "yom042.jpg" },
+{ name: "YOM043", category: "women dress", image: "yom043.jpg" },
+{ name: "YOM044", category: "women dress", image: "yom044.jpg" },
+{ name: "YOM045", category: "women dress", image: "yom045.jpg" },
+{ name: "YOM046", category: "women dress", image: "yom046.jpg" },
+{ name: "YOM047", category: "women dress", image: "yom047.jpg" },
+{ name: "YOM048", category: "women dress", image: "yom048.jpg" },
+{ name: "YOM049", category: "women dress", image: "yom049.jpg" },
+{ name: "YOM050", category: "women dress", image: "yom050.jpg" },
+{ name: "YOM051", category: "women dress", image: "yom051.jpg" },
+{ name: "YOM052", category: "women dress", image: "yom052.jpg" },
+{ name: "YOM053", category: "women dress", image: "yom053.jpg" },
+{ name: "YOM054", category: "women dress", image: "yom054.jpg" },
+{ name: "YOM055", category: "women dress", image: "yom055.jpg" },
+{ name: "YOM056", category: "women dress", image: "yom056.jpg" },
+{ name: "YOM057", category: "women dress", image: "yom057.jpg" },
+{ name: "YOM058", category: "women dress", image: "yom058.jpg" },
+{ name: "YOM059", category: "women dress", image: "yom059.jpg" },
+{ name: "YOM060", category: "women dress", image: "yom060.jpg" },
+{ name: "YOM061", category: "women dress", image: "yom061.jpg" },
+{ name: "YOM062", category: "women dress", image: "yom062.jpg" },
+{ name: "YOM063", category: "women dress", image: "yom063.jpg" },
+{ name: "YOM064", category: "women dress", image: "yom064.jpg" },
+{ name: "YOM065", category: "women dress", image: "yom065.jpg" },
+{ name: "YOM066", category: "women dress", image: "yom066.jpg" },
+{ name: "YOM067", category: "women dress", image: "yom067.jpg" },
+{ name: "YOM068", category: "women dress", image: "yom068.jpg" },
+{ name: "YOM069", category: "women dress", image: "yom069.jpg" },
+{ name: "YOM070", category: "women dress", image: "yom070.jpg" },
+{ name: "YOM071", category: "women dress", image: "yom071.jpg" },
+{ name: "YOM072", category: "women dress", image: "yom072.jpg" },
+{ name: "YOM073", category: "women dress", image: "yom073.jpg" },
+{ name: "YOM074", category: "women dress", image: "yom074.jpg" },
+{ name: "YOM075", category: "women dress", image: "yom075.jpg" },
+{ name: "YOM076", category: "women dress", image: "yom076.jpg" },
+{ name: "YOM077", category: "women dress", image: "yom077.jpg" },
+{ name: "YOM078", category: "women dress", image: "yom078.jpg" },
+{ name: "YOM079", category: "women dress", image: "yom079.jpg" },
+{ name: "YOM080", category: "women dress", image: "yom080.jpg" },
+{ name: "YOM081", category: "women dress", image: "yom081.jpg" },
+{ name: "YOM082", category: "women dress", image: "yom082.jpg" },
+{ name: "YOM083", category: "women dress", image: "yom083.jpg" },
+{ name: "YOM084", category: "women dress", image: "yom084.jpg" },
+{ name: "YOM085", category: "women dress", image: "yom085.jpg" },
+{ name: "YOM086", category: "women dress", image: "yom086.jpg" },
+{ name: "YOM087", category: "women dress", image: "yom087.jpg" },
+{ name: "YOM088", category: "women dress", image: "yom088.jpg" },
+{ name: "YOM089", category: "women dress", image: "yom089.jpg" },
+{ name: "YOM090", category: "women dress", image: "yom090.jpg" },
+{ name: "YOM091", category: "women dress", image: "yom091.jpg" },
+{ name: "YOM092", category: "women dress", image: "yom092.jpg" },
+{ name: "YOM093", category: "women dress", image: "yom093.jpg" },
+{ name: "YOM094", category: "women dress", image: "yom094.jpg" },
+{ name: "YOM095", category: "women dress", image: "yom095.jpg" },
+{ name: "YOM096", category: "women dress", image: "yom096.jpg" },
+{ name: "YOM097", category: "women dress", image: "yom097.jpg" },
+{ name: "YOM098", category: "women dress", image: "yom098.jpg" },
+{ name: "YOM099", category: "women dress", image: "yom099.jpg" },
+{ name: "YOM100", category: "women dress", image: "yom100.jpg" },
+{ name: "YOM101", category: "men dress", image: "yom101.jpg" },
+{ name: "YOM102", category: "men dress", image: "yom102.jpg" },
+{ name: "YOM103", category: "men dress", image: "yom103.jpg" },
+{ name: "YOM104", category: "men dress", image: "yom104.jpg" },
+{ name: "YOM105", category: "men dress", image: "yom105.jpg" },
+{ name: "YOM106", category: "men dress", image: "yom106.jpg" },
+{ name: "YOM107", category: "men dress", image: "yom107.jpg" },
+{ name: "YOM108", category: "men dress", image: "yom108.jpg" },
+{ name: "YOM109", category: "men dress", image: "yom109.jpg" },
+{ name: "YOM110", category: "men dress", image: "yom110.jpg" },
+{ name: "YOM111", category: "men dress", image: "yom111.jpg" },
+{ name: "YOM112", category: "men dress", image: "yom112.jpg" },
+{ name: "YOM113", category: "men dress", image: "yom113.jpg" },
+{ name: "YOM114", category: "men dress", image: "yom114.jpg" },
+{ name: "YOM115", category: "men dress", image: "yom115.jpg" },
+{ name: "YOM116", category: "men dress", image: "yom116.jpg" },
+{ name: "YOM117", category: "men dress", image: "yom117.jpg" },
+{ name: "YOM118", category: "men dress", image: "yom118.jpg" },
+{ name: "YOM119", category: "men dress", image: "yom119.jpg" },
+{ name: "YOM120", category: "men dress", image: "yom120.jpg" },
+{ name: "YOM121", category: "men dress", image: "yom121.jpg" },
+{ name: "YOM122", category: "men dress", image: "yom122.jpg" },
+{ name: "YOM123", category: "men dress", image: "yom123.jpg" },
+{ name: "YOM124", category: "men dress", image: "yom124.jpg" },
+{ name: "YOM125", category: "men dress", image: "yom125.jpg" },
+{ name: "YOM126", category: "men dress", image: "yom126.jpg" },
+{ name: "YOM127", category: "men dress", image: "yom127.jpg" },
+{ name: "YOM128", category: "men dress", image: "yom128.jpg" },
+{ name: "YOM129", category: "men dress", image: "yom129.jpg" },
+{ name: "YOM130", category: "men dress", image: "yom130.jpg" },
+{ name: "YOM131", category: "men dress", image: "yom131.jpg" },
+{ name: "YOM132", category: "men dress", image: "yom132.jpg" },
+{ name: "YOM133", category: "men dress", image: "yom133.jpg" },
+{ name: "YOM134", category: "men dress", image: "yom134.jpg" },
+{ name: "YOM135", category: "men dress", image: "yom135.jpg" },
+{ name: "YOM136", category: "men dress", image: "yom136.jpg" },
+{ name: "YOM137", category: "men dress", image: "yom137.jpg" },
+{ name: "YOM138", category: "men dress", image: "yom138.jpg" },
+{ name: "YOM139", category: "men dress", image: "yom139.jpg" },
+{ name: "YOM140", category: "men dress", image: "yom140.jpg" },
+{ name: "YOM141", category: "men dress", image: "yom141.jpg" },
+{ name: "YOM142", category: "men dress", image: "yom142.jpg" },
+{ name: "YOM143", category: "men dress", image: "yom143.jpg" },
+{ name: "YOM144", category: "men dress", image: "yom144.jpg" },
+{ name: "YOM145", category: "men dress", image: "yom145.jpg" },
+{ name: "YOM146", category: "men dress", image: "yom146.jpg" },
+{ name: "YOM147", category: "men dress", image: "yom147.jpg" },
+{ name: "YOM148", category: "men dress", image: "yom148.jpg" },
+{ name: "YOM149", category: "men dress", image: "yom149.jpg" },
+{ name: "YOM150", category: "men dress", image: "yom150.jpg" },
+{ name: "YOM151", category: "men dress", image: "yom151.jpg" },
+{ name: "YOM152", category: "men dress", image: "yom152.jpg" },
+{ name: "YOM153", category: "men dress", image: "yom153.jpg" },
+{ name: "YOM154", category: "men dress", image: "yom154.jpg" },
+{ name: "YOM155", category: "men dress", image: "yom155.jpg" },
+{ name: "YOM156", category: "men dress", image: "yom156.jpg" },
+{ name: "YOM157", category: "men dress", image: "yom157.jpg" },
+{ name: "YOM158", category: "men dress", image: "yom158.jpg" },
+{ name: "YOM159", category: "men dress", image: "yom159.jpg" },
+{ name: "YOM160", category: "men dress", image: "yom160.jpg" },
+{ name: "YOM161", category: "men dress", image: "yom161.jpg" },
+{ name: "YOM162", category: "men dress", image: "yom162.jpg" },
+{ name: "YOM163", category: "men dress", image: "yom163.jpg" },
+{ name: "YOM164", category: "men dress", image: "yom164.jpg" },
+{ name: "YOM165", category: "men dress", image: "yom165.jpg" },
+{ name: "YOM166", category: "men dress", image: "yom166.jpg" },
+{ name: "YOM167", category: "men dress", image: "yom167.jpg" },
+{ name: "YOM168", category: "men dress", image: "yom168.jpg" },
+{ name: "YOM169", category: "men dress", image: "yom169.jpg" },
+{ name: "YOM170", category: "men dress", image: "yom170.jpg" },
+{ name: "YOM171", category: "men dress", image: "yom171.jpg" },
+{ name: "YOM172", category: "men dress", image: "yom172.jpg" },
+{ name: "YOM173", category: "men dress", image: "yom173.jpg" },
+{ name: "YOM174", category: "men dress", image: "yom174.jpg" },
+{ name: "YOM175", category: "men dress", image: "yom175.jpg" },
+{ name: "YOM176", category: "men dress", image: "yom176.jpg" },
+{ name: "YOM177", category: "men dress", image: "yom177.jpg" },
+{ name: "YOM178", category: "men dress", image: "yom178.jpg" },
+{ name: "YOM179", category: "men dress", image: "yom179.jpg" },
+{ name: "YOM180", category: "men dress", image: "yom180.jpg" },
+{ name: "YOM181", category: "men dress", image: "yom181.jpg" },
+{ name: "YOM182", category: "men dress", image: "yom182.jpg" },
+{ name: "YOM183", category: "men dress", image: "yom183.jpg" },
+{ name: "YOM184", category: "men dress", image: "yom184.jpg" },
+{ name: "YOM185", category: "men dress", image: "yom185.jpg" },
+{ name: "YOM186", category: "men dress", image: "yom186.jpg" },
+{ name: "YOM187", category: "men dress", image: "yom187.jpg" },
+{ name: "YOM188", category: "men dress", image: "yom188.jpg" },
+{ name: "YOM189", category: "men dress", image: "yom189.jpg" },
+{ name: "YOM190", category: "men dress", image: "yom190.jpg" },
+{ name: "YOM191", category: "men dress", image: "yom191.jpg" },
+{ name: "YOM192", category: "men dress", image: "yom192.jpg" },
+{ name: "YOM193", category: "men dress", image: "yom193.jpg" },
+{ name: "YOM194", category: "men dress", image: "yom194.jpg" },
+{ name: "YOM195", category: "men dress", image: "yom195.jpg" },
+{ name: "YOM196", category: "men dress", image: "yom196.jpg" },
+{ name: "YOM197", category: "men dress", image: "yom197.jpg" },
+{ name: "YOM198", category: "men dress", image: "yom198.jpg" },
+{ name: "YOM199", category: "men dress", image: "yom199.jpg" },
+{ name: "YOM200", category: "men dress", image: "yom200.jpg" },
+{ name: "YOM201", category: "perfume", image: "yom201.jpg" },
+{ name: "YOM202", category: "perfume", image: "yom202.jpg" },
+{ name: "YOM203", category: "perfume", image: "yom203.jpg" },
+{ name: "YOM204", category: "perfume", image: "yom204.jpg" },
+{ name: "YOM205", category: "perfume", image: "yom205.jpg" },
+{ name: "YOM206", category: "perfume", image: "yom206.jpg" },
+{ name: "YOM207", category: "perfume", image: "yom207.jpg" },
+{ name: "YOM208", category: "perfume", image: "yom208.jpg" },
+{ name: "YOM209", category: "perfume", image: "yom209.jpg" },
+{ name: "YOM210", category: "perfume", image: "yom210.jpg" },
+{ name: "YOM211", category: "perfume", image: "yom211.jpg" },
+{ name: "YOM212", category: "perfume", image: "yom212.jpg" },
+{ name: "YOM213", category: "perfume", image: "yom213.jpg" },
+{ name: "YOM214", category: "perfume", image: "yom214.jpg" },
+{ name: "YOM215", category: "perfume", image: "yom215.jpg" },
+{ name: "YOM216", category: "perfume", image: "yom216.jpg" },
+{ name: "YOM217", category: "perfume", image: "yom217.jpg" },
+{ name: "YOM218", category: "perfume", image: "yom218.jpg" },
+{ name: "YOM219", category: "perfume", image: "yom219.jpg" },
+{ name: "YOM220", category: "perfume", image: "yom220.jpg" },
+{ name: "YOM221", category: "perfume", image: "yom221.jpg" },
+{ name: "YOM222", category: "perfume", image: "yom222.jpg" },
+{ name: "YOM223", category: "perfume", image: "yom223.jpg" },
+{ name: "YOM224", category: "perfume", image: "yom224.jpg" },
+{ name: "YOM225", category: "perfume", image: "yom225.jpg" },
+{ name: "YOM226", category: "perfume", image: "yom226.jpg" },
+{ name: "YOM227", category: "perfume", image: "yom227.jpg" },
+{ name: "YOM228", category: "perfume", image: "yom228.jpg" },
+{ name: "YOM229", category: "perfume", image: "yom229.jpg" },
+{ name: "YOM230", category: "perfume", image: "yom230.jpg" },
+{ name: "YOM231", category: "perfume", image: "yom231.jpg" },
+{ name: "YOM232", category: "perfume", image: "yom232.jpg" },
+{ name: "YOM233", category: "perfume", image: "yom233.jpg" },
+{ name: "YOM234", category: "perfume", image: "yom234.jpg" },
+{ name: "YOM235", category: "perfume", image: "yom235.jpg" },
+{ name: "YOM236", category: "perfume", image: "yom236.jpg" },
+{ name: "YOM237", category: "perfume", image: "yom237.jpg" },
+{ name: "YOM238", category: "perfume", image: "yom238.jpg" },
+{ name: "YOM239", category: "perfume", image: "yom239.jpg" },
+{ name: "YOM240", category: "perfume", image: "yom240.jpg" },
+{ name: "YOM241", category: "perfume", image: "yom241.jpg" },
+{ name: "YOM242", category: "perfume", image: "yom242.jpg" },
+{ name: "YOM243", category: "perfume", image: "yom243.jpg" },
+{ name: "YOM244", category: "perfume", image: "yom244.jpg" },
+{ name: "YOM245", category: "perfume", image: "yom245.jpg" },
+{ name: "YOM246", category: "perfume", image: "yom246.jpg" },
+{ name: "YOM247", category: "perfume", image: "yom247.jpg" },
+{ name: "YOM248", category: "perfume", image: "yom248.jpg" },
+{ name: "YOM249", category: "perfume", image: "yom249.jpg" },
+{ name: "YOM250", category: "perfume", image: "yom250.jpg" },
+];
+function showProducts(list){
+
+const container = document.getElementById("productList");
+
+container.innerHTML = "";
+
+list.forEach(product => {
+
+container.innerHTML += `
+
+<div class="card">
+
+<img src="${product.image}" alt="${product.name}" onclick="event.stopPropagation(); zoomImage('${product.image}')">
+
+<h3>${product.name}</h3>
+
+<p>${product.category}</p>
+
+<button onclick="event.stopPropagation(); order('${product.name}')">
+Order on WhatsApp
+</button>
+
+</div>
+
+`;
+
+});
+
+}
+
+showProducts(products);
+
+// SEARCH
+function searchProduct(){
+
+const value = document.getElementById("search").value.toLowerCase();
+
+const result = products.filter(product =>
+
+product.name.toLowerCase().includes(value) ||
+
+product.category.toLowerCase().includes(value)
+
+);
+
+showProducts(result);
+
+}
+
+// CATEGORY FILTER
+function filterCategory(category){
+
+if(category === "all"){
+
+showProducts(products);
+
+return;
+
+}
+
+const result = products.filter(product =>
+product.category.toLowerCase() === category.toLowerCase()
+);
+
+showProducts(result);
+
+}
+
+// WHATSAPP ORDER
+function order(name){
+
+const message = `Hello, I want to order ${name}`;
+
+window.open(
+`https://wa.me/919507059053?text=${encodeURIComponent(message)}`
+);
+
+}
+
+// IMAGE ZOOM
+function zoomImage(image){
+
+const popup = document.createElement("div");
+
+popup.style.position = "fixed";
+popup.style.left = "0";
+popup.style.top = "0";
+popup.style.width = "100%";
+popup.style.height = "100%";
+popup.style.background = "rgba(0,0,0,.9)";
+popup.style.display = "flex";
+popup.style.justifyContent = "center";
+popup.style.alignItems = "center";
+popup.style.zIndex = "9999";
+
+popup.innerHTML = `
+<img src="${image}" style="max-width:90%;max-height:90%;border-radius:10px;">
+`;
+
+popup.onclick = function(){
+popup.remove();
+}
+
+document.body.appendChild(popup);
+
+}
